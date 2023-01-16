dc2d253b8fcc01b2d26253af0bdfccedbb9382e9
Yuriy <sol-yuriy@yandex.ru>
2023-01-16 17:56:42 +0500

    Определение названия дня недели

---
+++ Ex03/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Write("Введите номер дня недели: ");
+string number = Console.ReadLine() ?? "";
+
+double num = double.Parse(number);
+
+if (num == 1)
+{
+    Console.Write($"{num} --> Понедельник");
+}
+else if (num == 2)
+{
+    Console.Write($"{num} --> Вторник");
+}
+else if (num == 3)
+{
+    Console.Write($"{num} --> Среда");
+}
+else if (num == 4)
+{
+    Console.Write($"{num} --> Четверг");
+}
+else if (num == 5)
+{
+    Console.Write($"{num} --> Пятница");
+}
+else if (num == 6)
+{
+    Console.Write($"{num} --> Суббота");
+}
+else if (num == 7)
+{
+    Console.Write($"{num} --> Воскресенье");
+}
