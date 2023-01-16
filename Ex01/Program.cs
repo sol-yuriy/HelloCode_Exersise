cac7cf6f6df7bd473a85ed9a1b5b6ccef8195953
Yuriy <sol-yuriy@yandex.ru>
2023-01-16 13:01:54 +0500

    Задача на возведение в степень

---
+++ Ex01/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("введите число: ");
+//string userInput = Console.ReadLine() ?? "";
+
+//int number = int.Parse(userInput);
+int number = int.Parse(Console.ReadLine() ?? "");
+
+//int result = (int)Math.Pow(number, 3);
+double result = Math.Pow(number, 3);
+//int result = number ^ 2;
+
+Console.Write($"{number} в квадрате -> {result}");
+
