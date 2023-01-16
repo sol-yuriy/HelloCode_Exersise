793f384ead247a0452f5674ee235754d7e69d914
Yuriy <sol-yuriy@yandex.ru>
2023-01-16 23:21:50 +0500

    Принимаем число (N), а на выходе показываем все чётные числа от 1 до N.

---
+++ Ex08/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+string num = Console.ReadLine() ?? "";
+int number = int.Parse(num);
+int i = 1;
+while (i < number)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine($"{i}");
+    }
+i++;
+}
