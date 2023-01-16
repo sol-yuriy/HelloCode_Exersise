6ca170ae2810b7f1ba64cc0ee395fd502d1b494f
Yuriy <sol-yuriy@yandex.ru>
2023-01-16 22:34:53 +0500

    Четное / нечетное

---
+++ Ex06/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число на проверку четности: ");
+string num = Console.ReadLine() ?? "";
+double number = double.Parse(num);
+if (number % 2 == 0)
+{
+    Console.Write($"Число {number} четное");
+}
+else
+{
+    Console.Write($"Число {number} НЕчетное");
+}
