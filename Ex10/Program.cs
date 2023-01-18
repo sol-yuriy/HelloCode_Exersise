a92f0a5e7ce9cf7effb72b2c3a8368abd8c658ad
Yuriy <sol-yuriy@yandex.ru>
2023-01-18 14:40:28 +0500

    Ввод случ.числа и опред-е макс цифры в нем

---
+++ Ex10/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа. 78 -> 8, 12-> 2, 85 -> 8
+
+var rnd = new Random();
+int num = rnd.Next(10, 100);
+
+int a1 = num / 10;
+int a2 = num % 10;
+
+//int max=a1;
+//if (max < a2) max = a2;
+
+int max = (a1 < a2) ? a2 : a1;
+
+Console.WriteLine($"{num} --> {max}");
