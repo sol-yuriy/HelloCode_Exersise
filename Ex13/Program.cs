d219c2cd90a55e86885a9e34bdaad088841c2d50
Yuriy <sol-yuriy@yandex.ru>
2023-01-18 17:50:02 +0500

    Ввести любое целое число и вывести 3ю цифру

---
+++ Ex13/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.Write("Введите число: ");
+string numb = Console.ReadLine() ?? "";
+int num = int.Parse(numb);
+
+int divMillion = num % 1000000;
+
+Console.WriteLine($"{divMillion}");
+
+if (divMillion > 10)
+{
+    int divMillion = num % 10000000;
+}
