4b2bba8753c8a506739a1cf0bb8634eb6b73354c
Yuriy <sol-yuriy@yandex.ru>
2023-01-18 17:24:43 +0500

    Вторая цифра 3х значного числа

---
+++ Ex10.1/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите 3х значное число: ");
+string numb=Console.ReadLine() ?? "";
+int num = int.Parse(numb);
+
+//int a1 = rnd / 100;
+int a2 = (num / 10) % 10;
+//int a3 = rnd % 10;
+
+Console.WriteLine($"{num} : {a2}");
