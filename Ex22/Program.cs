17d2f0698aa493956d142714fbbf0d14d9063791
Yuriy <sol-yuriy@yandex.ru>
2023-01-21 19:30:14 +0500

    Вывод списка квадратов

---
+++ Ex22/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Задача 22: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+5 -> 1, 4, 9, 16, 25.
+2 -> 1,4  
+*/
+
+Console.Write("Введите целое число: ");
+int N = int.Parse(Console.ReadLine() ?? "");
+int numbers =1;
+
+Console.Write($"{N} --> ");
+while (numbers <= N)
+{
+    Console.Write($" {Math.Pow(numbers, 2)}, ");
+    numbers++; 
+}
