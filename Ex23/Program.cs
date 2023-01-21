6aa3c1b1c8ac3f78f25ffd18b16355a6d67d90d6
Yuriy <sol-yuriy@yandex.ru>
2023-01-22 00:09:07 +0500

    Куб числа

---
+++ Ex23/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 4, 9. 
+5 -> 1, 8, 27, 64, 125
+*/
+
+Console.Write("Введите целое число: ");
+int N = int.Parse(Console.ReadLine() ?? "");
+int numbers =1;
+
+Console.Write($"{N} --> ");
+while (numbers <= Math.Abs(N))
+{
+    Console.Write($" {Math.Pow(numbers, 3)}, ");
+    numbers++; 
+}
