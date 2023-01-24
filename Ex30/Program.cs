425a4bb83b1fad885a72667d2bb9885cb72b3902
Yuriy <sol-yuriy@yandex.ru>
2023-01-24 12:37:18 +0500

    Задача 30. Массив из 8 элементов, заполненный 0 и 1 в случайном порядке

---
+++ Ex30/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0]
+*/
+
+Console.Clear();
+
+int num = GetNumberFromUser("Введите целое число A: ", "Ошибка ввода!");
+
+int i = 0;
+int[] element = new int[num];
+Console.Write("[ ");
+while (i < num)
+{
+    element [i] = new Random().Next(0, 2);
+    Console.Write($"{element[i]} ");
+    i++;
+}
+Console.Write("]");
+
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return Math.Abs(userNumber);
+        Console.WriteLine(errorMessage);
+    }
+}
