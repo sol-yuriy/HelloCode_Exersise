c1453b012186f4a295bd7a0d559ab458b0169f0f
Yuriy <sol-yuriy@yandex.ru>
2023-01-24 10:04:01 +0500

    Выдаёт количество цифр в числе

---
+++ Ex24.1/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5
+*/
+
+Console.Clear();
+int num = GetNumberFromUser("Введите целое число A: ", "Ошибка ввода!");
+int countNum = GetCountNumbers(num);
+Console.WriteLine($"{num} -> {countNum}");
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int GetCountNumbers(int count)
+{
+    int i = 0;
+    while (true)
+    {
+        count = count / 10;
+        i++;
+        if (count == 0) break;
+    }
+    return i;
+}
