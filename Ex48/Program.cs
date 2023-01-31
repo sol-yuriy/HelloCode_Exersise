1d7c22beee60d24305b0346f9498a3372af24ac6
Yuriy <sol-yuriy@yandex.ru>
2023-01-31 12:02:04 +0500

    Задача 48. Создать двумерный массив с элементами A(ij) = i + j

---
+++ Ex48/Program.cs
@@ -0,0 +1,53 @@
+﻿/*Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+m = 3, n = 4.
+0 1 2 3
+1 2 3 4
+2 3 4 5
+*/
+
+int row = GetNumberFromUser("Введите количество строк: ", "Ошибка ввода!");
+int column = GetNumberFromUser("Введите количество столбцов: ", "Ошибка ввода!");
+int[,] result = GetFillArray(row, column);
+GetPrintArray(result, row, column);
+
+
+
+//Метод ввода целого числа пользователем
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+
+//Метод создания и заполнения массива по заданному условию
+int[,] GetFillArray(int row, int column)
+{
+    int [,] array = new int[row, column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            array[i,j] = i + j;
+        }
+    }
+    return array;
+}
+
+void GetPrintArray(int[,] array, int row, int column)
+{
+    for(int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
