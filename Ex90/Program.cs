d1414b826139776af1735b55ce9aea89b671105d
Yuriy <sol-yuriy@yandex.ru>
2023-02-17 21:04:55 +0500

    Итоговая работа. Формирование строчного массива.2

--- Ex90/Program.cs
+++ Ex90/Program.cs
@@ -2,13 +2,26 @@
 Написать программу, которая из имеющегося массива строк сформирует новый массив строк, длина которых меньше либо равна 3.
 */
 
-string[] array = { "dhgdfhdf", "df9", "5", "7gtrg5.", "398hf", "jhbhf", "/._1", "jhgytf545e" };
+string[] array = { "dhgdfhdf", "df9", "5", "7gt", "398hf", "jhbhf", "/._", "jhgytf545e" };
 
 // определим длину результирующего массива
 int size=0;
 for (int i = 0; i < array.Length; i++)
     if (array[i].Length <= 3)  size++;
 
+// сформируем новый массив
+string[] part = new string[size];
+int j=0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        part[j] = array[i];
+        j++;
+    }
+}
+
+Console.WriteLine($"{String.Join(", ", part)}");
 
 
 
