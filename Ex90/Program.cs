10e3f13e06900ad07b1f944267ea90921dc11bf5
Yuriy <sol-yuriy@yandex.ru>
2023-02-17 22:10:10 +0500

    Итоговая работа. Формирование строчного массива.Ручной ввод массива.4

--- Ex90/Program.cs
+++ Ex90/Program.cs
@@ -2,6 +2,33 @@
 Написать программу, которая из имеющегося массива строк сформирует новый массив строк, длина которых меньше либо равна 3.
 */
 
+int size = GetNumberFromUser("Введите количество элементов массива': ", "Ошибка ввода!");
+
+
+
+
+// Метод ввода целого числа пользователем
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+// Метод формирования начального строкового массива
+string[] GetStringArray(int size)
+{
+    string[] array = new string[size];
+    for (int i=0; i<size;i++)
+        array[i] = Console.ReadLine() ?? "";
+    return array;
+}
+
 string[] array = { "dhgdfhdf", "df9", "5", "7gt", "398hf", "jhbhf", "/._", "jhgytf545e" };
 
 // определим длину результирующего массива
